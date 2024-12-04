475b91fc6b18aab91363f7a64237d3663c459b97
Oluwatoni Ogunmade <oogunmade3@gatech.edu>
2024-12-04 14:04:30 -0500

    Complete part 2

--- 2024/day_1/day_1.cpp
+++ 2024/day_1/day_1.cpp
@@ -5,19 +5,23 @@
 #include <vector>
 #include <sstream>
 #include <string>
-
+#include <unordered_map>
 
 int main()
 {
+    // For the first half
     // Create the two min heaps(queues)
     std::priority_queue<int, std::vector<int>, std::greater<int> > left_pq, right_pq;
 
+    // For the second half
+    std::vector<int> left_list;
+    std::unordered_map<int, size_t> right_counter;
+
     // Load the input file
-    std::ifstream file("test_2.txt");
+    std::ifstream file("test_1.txt");
     std::string line;
 
-    // Build the min heaps as the file is loaded
-    while(std::getline(file, line))
+    while (std::getline(file, line))
     {
         std::stringstream linestream(line);
         std::string data;
@@ -25,9 +29,23 @@ int main()
 
         linestream >> left_val;
         linestream >> right_val;
-        
+
+        // Build the min heaps as the file is loaded
         left_pq.push(left_val);
         right_pq.push(right_val);
+
+        // Build the right dictionary to count the occurence of each ID
+        if (right_counter.find(right_val) == right_counter.end())
+        {
+            right_counter[right_val] = 1;
+        }
+        else
+        {
+            right_counter[right_val]++;
+        }
+
+        // Build the left list
+        left_list.push_back(left_val);
     }
     file.close();
 
@@ -45,5 +63,17 @@ int main()
     }
 
     std::cout << "total sum: " << distance_sum << std::endl;
+
+    // For the second part
+    int similarity_score = 0;
+    for (int val : left_list)
+    {
+        if (right_counter.find(val) != right_counter.end())
+        {
+            similarity_score += val * right_counter[val];
+        }
+    }
+
+    std::cout << "similarity score: " << similarity_score << std::endl;
     return 0;
 }
